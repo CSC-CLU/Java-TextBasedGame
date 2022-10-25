27d5dce0c244d14a28a9bd5203439a56aa50edd3
sudo-Eric <ericheinke0@gmail.com>
2022-10-24 18:54:56 -0700

    Added initial graphics code from testing

---
+++ lib/ANSI_Escape_Code_Creator.jar.md
@@ -0,0 +1,1 @@
+Please download the latest version of the [ANSI Escape Code Creator](https://github.com/sudo-Eric/ANSI-Escape-Code-Creator/releases/latest) and place it in this folder.
---
+++ src/MainApp.java
@@ -0,0 +1,8 @@
+import ANSI_Escape_Code_Creator.ANSI_Escape_Code_Creator;
+import static ANSI_Escape_Code_Creator.ANSI_Escape_Code_Creator_Static.*;
+
+public class MainApp {
+    public static void main(String[] args) throws InterruptedException {
+        GraphicsTestApp.main(args);
+    }
+}
